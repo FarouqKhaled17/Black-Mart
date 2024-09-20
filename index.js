50d0af5924e9711124e19c50eb48554cbb282dc1
Farouk Khaled <80914530+FarouqKhaled17@users.noreply.github.com>
2024-09-20 08:08:51 +0300

    cors and other modifications in products apis

--- index.js
+++ index.js
@@ -10,9 +10,11 @@ import { config } from "dotenv";
 import { bootstrap } from "./src/modules/index.routes.js";
 import { AppError } from "./src/utils/AppError.js";
 import { globalError } from "./src/middleware/globalError.js";
+import cors from "cors";
 const app = express();
 app.use(express.json());
-app.use('/uploads',express.static('uploads'))
+app.use(cors());
+app.use('/uploads', express.static('uploads'))
 
 dbConnection();
 bootstrap(app);

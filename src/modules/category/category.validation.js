37a4b21fb4abb682acde2d713714ae5b6e581e50
Farouk Khaled <80914530+FarouqKhaled17@users.noreply.github.com>
2024-09-22 18:06:38 +0300

    fixed bug

--- src/modules/category/category.validation.js
+++ src/modules/category/category.validation.js
@@ -8,7 +8,7 @@ const addNewCategoryVal = joi.object({
         originalname: joi.string().required(),
         encoding: joi.string().required(),
         mimetype: joi.string().valid('image/png', 'image/jpg', 'image/jpeg').required(),
-        size: joi.number().max(5242880).required(), 
+        size: joi.number().max(5242880).required(),
         destination: joi.string().required(),
         filename: joi.string().required(),
         path: joi.string().required(),
@@ -26,7 +26,17 @@ const deleteCategoryVal = joi.object({
 //validation for updating a category
 const updateCategoryVal = joi.object({
     id: joi.string().required().hex().length(24),
-    name: joi.string().min(2).max(100).trim()
+    name: joi.string().min(2).max(100).trim(),
+    image: joi.object({
+        fieldname: joi.string().required(),
+        originalname: joi.string().required(),
+        encoding: joi.string().required(),
+        mimetype: joi.string().valid('image/png', 'image/jpg', 'image/jpeg').required(),
+        size: joi.number().max(5242880).required(),
+        destination: joi.string().required(),
+        filename: joi.string().required(),
+        path: joi.string().required(),
+    }).required()
 })
 
 
